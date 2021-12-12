310c41bba725787f13606d46f06ab2420df53c1b
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2021-12-13 02:42:27 +0530

    Added the first comprehension

---
+++ List Comprehensions.py
@@ -0,0 +1,8 @@
+# Author -- Saurabh Kumar Gupta                                                                                            Date -- 13 dec 2021
+
+# This Python File shows how list comprehensions can save you a lot of time and frustations with the creation of lists. 
+
+# 1. Making a list that goes from 0 to 150 in one line, and printing it in another. 
+
+list1 = [i for i in range(151)]
+print(list1)
