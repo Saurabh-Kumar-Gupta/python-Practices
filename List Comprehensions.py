a79e9c8ed09e773661adabec81cc14cc108ffae3
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2021-12-13 03:05:34 +0530

    shortened and made it more easy on the eyes

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -2,19 +2,20 @@
 
 # This Python File shows how list comprehensions can save you a lot of time and frustations with the creation of lists. 
 
-# 1. Making a list that goes from 0 to 150 in one line
+# 1. Making a list that goes from 1 to 15 in one line
+print("\n \n NO.1 - Creating a normal list comprehension which get me values from 1 to 15 in one line. ")
 
-print([i for i in range(15)])
+print([i for i in range(1,15)])              # we can get a whole list with just one line
 
 
 # 2. Doing math with your comprehensions. 
-print("\n \n N0.2 Doing math with List comprehensions \n \n")
+print("\n \n N0.2 Doing math with List comprehensions \n")
 
 print([i*10 for i in range(1,11)])            # every element will be multiplied by 10 
 
 
 
 #3. Making it even more intresting by having a conditional statement. 
-print("\n \n NO.3 using a conditional statement to have more control over the comprehensions \n \n")
+print("\n \n NO.3 using a conditional statement to have more control over the comprehensions \n")
 
 print([i for i in range(10,210) if i%10 == 0])    # every element will be tested against the specific condition you put. 
