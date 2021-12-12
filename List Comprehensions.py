3dce332cad715d8b1695e50abc1d355954576277
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2021-12-13 03:01:44 +0530

    added a third comprehension and shortened the length of the program by printing the list at the same line

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -2,13 +2,19 @@
 
 # This Python File shows how list comprehensions can save you a lot of time and frustations with the creation of lists. 
 
-# 1. Making a list that goes from 0 to 150 in one line, and printing it in another. 
+# 1. Making a list that goes from 0 to 150 in one line
 
-list1 = [i for i in range(151)]
-print(list1)
+print([i for i in range(15)])
+
 
 # 2. Doing math with your comprehensions. 
 print("\n \n N0.2 Doing math with List comprehensions \n \n")
 
-list2 = [i*10 for i in range(1,101)]            # every element will be multiplied by 10 
-print(list2)
+print([i*10 for i in range(1,11)])            # every element will be multiplied by 10 
+
+
+
+#3. Making it even more intresting by having a conditional statement. 
+print("\n \n NO.3 using a conditional statement to have more control over the comprehensions \n \n")
+
+print([i for i in range(10,210) if i%10 == 0])    # every element will be tested against the specific condition you put. 
