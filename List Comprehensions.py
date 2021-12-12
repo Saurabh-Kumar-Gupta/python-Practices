91c6695e81eeb039d0703cc2f7ba88fd828804ab
Saurabh-Kumar-Gupta <saurabh.gupta.6174@gmail.com>
2021-12-13 03:12:34 +0530

    Making the function list comprehension

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -3,7 +3,7 @@
 # This Python File shows how list comprehensions can save you a lot of time and frustations with the creation of lists. 
 
 # 1. Making a list that goes from 1 to 15 in one line
-print("\n \n NO.1 - Creating a normal list comprehension which get me values from 1 to 15 in one line. ")
+print("\n NO.1 - Creating a normal list comprehension which get me values from 1 to 15 in one line. \n")
 
 print([i for i in range(1,15)])              # we can get a whole list with just one line
 
@@ -19,3 +19,12 @@ print([i*10 for i in range(1,11)])            # every element will be multiplied
 print("\n \n NO.3 using a conditional statement to have more control over the comprehensions \n")
 
 print([i for i in range(10,210) if i%10 == 0])    # every element will be tested against the specific condition you put. 
+
+
+#4. Getting bonanza with the comprehensions and using a function to make the list. 
+print("\n \n Using a function to create a list for ultimate list customization. \n")
+
+def Lst_Function(i):
+    i
+print([Lst_Function(i) for i in range(1,10)])
+
